786c244ff045a951195f82206ba60c282a089bfb
PRAVIN PILODIYA <p143nimcet@gmail.com>
2024-01-23 17:28:47 +0530

    backend url added

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import "../../styles/AuthStyles.css";
+
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -12,13 +13,12 @@ const Register = () => {
   const [address, setAddress] = useState("");
   const [answer, setAnswer] = useState("");
   const navigate = useNavigate();
-  const route_url=process.env.ROUTE_URL;
+  // const route_url=process.env.ROUTE_URL;
   // form function
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-
-      const res = await axios.post(`/api/v1/auth/register`, {
+      const res = await axios.post(`https://mithaicart-qsip.vercel.app/api/v1/auth/register`, {
         name,
         email,
         password,
