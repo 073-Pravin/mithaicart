1d2a9692146786ca380c0a965e8275bea23383eb
PRAVIN PILODIYA <p143nimcet@gmail.com>
2024-03-05 19:05:31 +0530

    fixed proxy

--- client/src/components/Form/SearchInput.js
+++ client/src/components/Form/SearchInput.js
@@ -10,7 +10,7 @@ const SearchInput = () => {
     e.preventDefault();
     try {
       const { data } = await axios.get(
-        `https://mithaicart-3nna.vercel.app//api/v1/product/search/${values.keyword}`
+        `https://mithaicart-3nna.vercel.app/api/v1/product/search/${values.keyword}`
       );
       setValues({ ...values, results: data });
       navigate("/search");
