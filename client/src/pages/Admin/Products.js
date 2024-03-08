2f6268cd47d10c73609cb7fc5a8b31851429d134
PRAVIN PILODIYA <p143nimcet@gmail.com>
2024-03-08 13:22:02 +0530

    fixed: some proxy issues

--- client/src/pages/Admin/Products.js
+++ client/src/pages/Admin/Products.js
@@ -34,12 +34,12 @@ const Products = () => {
             {products?.map((p) => (
               <Link
                 key={p._id}
-                to={`/dashboard/admin/product/${p.slug}`}
+                to={`${process.env.REACT_APP_YOUR_BACKEND_URL}/dashboard/admin/product/${p.slug}`}
                 className="product-link"
               >
                 <div className="card m-2" style={{ width: "18rem" }}>
                   <img
-                    src={`/api/v1/product/product-photo/${p._id}`}
+                    src={`${process.env.REACT_APP_YOUR_BACKEND_URL}/api/v1/product/product-photo/${p._id}`}
                     className="card-img-top"
                     alt={p.name}
                   />
