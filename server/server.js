833a71590485dead464796f29d681e0663462094
PRAVIN PILODIYA <p143nimcet@gmail.com>
2024-03-08 16:00:32 +0530

    cors issue update

--- server/server.js
+++ server/server.js
@@ -19,11 +19,11 @@ const app = express();
 
 //middelwares
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-  });
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   next();
+//   });
 app.use(express.json());
 app.use(morgan("dev"));
 
